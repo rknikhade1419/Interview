7307fefd9cc3d9a5e4ccb900ae6b1632583ac1b2
rknikhade1419 <nikhaderoshankumar@gmail.com>
2025-12-26 17:12:13 +0530

    Vpc Setup Added

--- terraform_rds/output.tf
+++ terraform_rds/output.tf
@@ -2,5 +2,5 @@ output "subnet_data"{
     value = [aws_subnet.db_subnet_1.id, aws_subnet.db_subnet_2.id]
 }
 output "rds_address"{
-    value = aws_db_instance.db_instance.address 
+   value = aws_db_instance.db_instance.address 
 }
